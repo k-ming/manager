b2f7c8bee5187e7d1bf52bcc93f3806465cb9efc
zh88168@yeah.net <zh88168@yeah.net>
2019-05-13 14:43:58 +0800

    修改

--- TestManager/managerTest.py
+++ TestManager/managerTest.py
@@ -48,24 +48,24 @@ if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     '''创建测试套件, 此处一定要控制用例执行顺序,tesRobotLogin必须首先执行,否则后面的用例会报错,找不到元素,并且unittest.main()方法执行用例是按照用例字母的顺序来执行的'''
     suite = unittest.TestSuite()
-    # suite.addTest(TestManager('testRobotLogin'))
-    # suite.addTest(TestManager('testAddRobot'))
-    # suite.addTest(TestManager('testRobotFace'))
+    suite.addTest(TestManager('testRobotLogin'))
+    suite.addTest(TestManager('testAddRobot'))
+    suite.addTest(TestManager('testRobotFace'))
     suite.addTest(TestManager('testHoloLogin'))
-    # suite.addTest(TestManager('testHoloLeftMenu'))
+    suite.addTest(TestManager('testHoloLeftMenu'))
 
 
     # 创建运行器, 并生成测试报告
-    # now_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    # filename = "管理后台回归测试" + "_Test_" + now_time + ".html"
-    # fp = open(filename, 'wb+')
-    # runner = HTMLTestRunner_cn.HTMLTestRunner(
-    #     stream=fp,
-    #     title='管理后台回归测试报告',
-    #     description='全息、机器人管理后台回归测试')
-    # runner.run(suite)
-    # fp.close()
+    now_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    filename = "管理后台回归测试" + "_Test_" + now_time + ".html"
+    fp = open(filename, 'wb+')
+    runner = HTMLTestRunner_cn.HTMLTestRunner(
+        stream=fp,
+        title='管理后台回归测试报告',
+        description='全息、机器人管理后台回归测试')
+    runner.run(suite)
+    fp.close()
 
-    runer = unittest.TextTestRunner()
-    runer.run(suite)
+    # runer = unittest.TextTestRunner()
+    # runer.run(suite)
 
